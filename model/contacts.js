bf78a342c1fc1b13dfbe2cc6316ded6eb6d6eaad
Irina-Koksharova <biodgerelo@ukr.net>
2021-02-19 14:47:12 +0200

    Adds functionality for work with contacts.json

---
+++ model/contacts.js
@@ -0,0 +1,40 @@
+const db = require('./db')
+const { v4: uuid } = require('uuid')
+
+const listContacts = async () => {
+  return db.get('contacts').value()
+}
+
+const getContactById = async (contactId) => {
+  return db.get('contacts').find({ contactId }).value()
+}
+
+const removeContact = async (contactId) => {
+  const [contactToRemove] = db.get('contacts').find({ contactId }).wtite()
+  return contactToRemove
+}
+
+const addContact = async (body) => {
+  const id = uuid()
+  const contactToAdd = {
+    id,
+    ...body,
+    ...(body.areСolleagues ? {} : { areСolleagues: true })
+  }
+  db.get('contacts').push(contactToAdd).write()
+  return contactToAdd
+}
+
+const updateContact = async (contactId, body) => {
+  const contactToUpdate = db.get('contacts').find({ contactId }).assign(body).value()
+  db.write()
+  return contactToUpdate.contactId ? contactToUpdate : null
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,
+}
